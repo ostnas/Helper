c69d7d0a3f5bc4d38157850d95ea4d65b8802618
nastya <ostnas95@gmail.com>
2021-04-15 21:36:22 +0700

    improved version

--- cases_utils.py
+++ cases_utils.py
@@ -1,57 +1,37 @@
 import copy
 
 
-def dict_without_key(dict, text):
-    keys = text.split('.')
-    new_dict = copy.deepcopy(dict)
-    next = new_dict
-    if keys[0] in next:
-        for key in keys[:-1]:
-            next = next[key]
-        if keys[-1] in next:
-            del next[keys[-1]]
-        else:
+def find_element(original_dict, keys_list):
+    next_dict = original_dict
+    for key in keys_list[:-1]:
+        if key not in next_dict:
             return None
-        return new_dict
-    else:
-        return None
+        next_dict = next_dict[key]
+    return next_dict
 
 
-def change_value_by_key(dict, text, value):
+def dict_without_key(original_dict, text):
     keys = text.split('.')
-    new_dict = copy.deepcopy(dict)
-    next = new_dict
-    if keys[0] in next:
-        for key in keys[:-1]:
-            next = next[key]
-        if keys[-1] in next:
-            next[keys[-1]] = value
-        else:
-            return None
-        return new_dict
-    else:
-        return None
+    new_dict = copy.deepcopy(original_dict)
+    next_dict = find_element(new_dict, keys)
+    assert(next_dict is not None and keys[-1] in next_dict), f'Key "{text}" not found.'
+    del next_dict[keys[-1]]
+    return new_dict
 
 
-def add_key_to_dict(dict, text, value):
+def dict_with_edited_value(original_dict, text, value):
     keys = text.split('.')
-    new_dict = copy.deepcopy(dict)
-    next = new_dict
+    new_dict = copy.deepcopy(original_dict)
+    next_dict = find_element(new_dict, keys)
+    assert(next_dict is not None and keys[-1] in next_dict), f'Key "{text}" not found.'
+    next_dict[keys[-1]] = value
+    return new_dict
 
-    if len(keys) == 1 and keys[0] not in next:
-        next[keys[0]] = value
-        return new_dict
-    elif len(keys) == 1 and keys[0] in next:
-        return None
 
-    for key in keys[:-1]:
-        if key in next:
-            next = next[key]
-        else:
-            return None
-
-    if keys[-1] in next:
-        return None
-    else:
-        next[keys[-1]] = value
-        return new_dict
+def dict_with_new_key(original_dict, text, value):
+    keys = text.split('.')
+    new_dict = copy.deepcopy(original_dict)
+    next_dict = find_element(new_dict, keys)
+    assert(next_dict is not None and keys[-1] not in next_dict), f'Key "{text}" already exists or path to key is incorrect'
+    next_dict[keys[-1]] = value
+    return new_dict
--- test/test_cases_utils.py
+++ test/test_cases_utils.py
@@ -1,255 +1,306 @@
-import pytest
-from cases_utils import dict_without_key
-from cases_utils import add_key_to_dict
-from cases_utils import change_value_by_key
-
-dict = {
-    "one": 1,
-    "two": "smth",
-    "three": True,
-    "five": {
-        "one": {
-            "two": 2,
-            "test": 3
-        }
-    }
-}
-
-TEST_SETUP = {
-    "delete": [
-        (
-            dict_without_key(dict, "text"),
-            None
-        ),
-        (
-            dict_without_key(dict, "five"),
-            {
-                "one": 1,
-                "two": "smth",
-                "three": True
-            }
-        ),
-        (
-            dict_without_key(dict, "five.one"),
-            {
-                "one": 1,
-                "two": "smth",
-                "three": True,
-                "five": {}
-            }
-        ),
-        (
-            dict_without_key(dict, "five.two"),
-            None
-        ),
-        (
-            dict_without_key(dict, "five.one.two"),
-            {
-                "one": 1,
-                "two": "smth",
-                "three": True,
-                "five": {
-                    "one": {
-                        "test": 3
-                    }
-                }
-            }
-        ),
-        (
-            dict_without_key(dict, ""),
-            None
-        ),
-        (
-            dict_without_key({}, "text"),
-            None
-        ),
-        (
-            dict_without_key({}, "five"),
-            None
-        ),
-        (
-            dict_without_key({}, "five.one"),
-            None
-        ),
-        (
-            dict_without_key({}, "five.two"),
-            None
-        ),
-        (
-            dict_without_key({}, "five.one.two"),
-            None
-        ),
-        (
-            dict_without_key({}, ""),
-            None
-        ),
-
-    ],
-    "change": [
-        (
-            change_value_by_key(dict, "text", "new"),
-            None
-        ),
-        (
-            change_value_by_key(dict, "five", "new"),
-            {
-                "one": 1,
-                "two": "smth",
-                "three": True,
-                "five": "new"
-            }
-        ),
-        (
-            change_value_by_key(dict, "five.one", "new"),
-            {
-                "one": 1,
-                "two": "smth",
-                "three": True,
-                "five": {
-                    "one": "new"
-                }
-            }
-        ),
-        (
-            change_value_by_key(dict, "five.two", "new"),
-            None
-        ),
-        (
-            change_value_by_key(dict, "five.one.two", "new"),
-            {
-                "one": 1,
-                "two": "smth",
-                "three": True,
-                "five": {
-                    "one": {
-                        "two": "new",
-                        "test": 3
-                    }
-                }
-            }
-        ),
-        (
-            change_value_by_key(dict, "", "new"),
-            None
-        ),
-        (
-            change_value_by_key({}, "text", "new"),
-            None
-        ),
-        (
-            change_value_by_key({}, "five", "new"),
-            None
-        ),
-        (
-            change_value_by_key({}, "five.one", "new"),
-            None
-        ),
-        (
-            change_value_by_key({}, "five.two", "new"),
-            None
-        ),
-        (
-            change_value_by_key({}, "five.one.two", "new"),
-            None
-        ),
-        (
-            change_value_by_key({}, "", "new"),
-            None
-        )
-    ],
-    "add": [
-        (
-            add_key_to_dict(dict, "text", "new"),
-            {
-                "one": 1,
-                "two": "smth",
-                "three": True,
-                "five": {
-                    "one": {
-                        "two": 2,
-                        "test": 3
-                    }
-                },
-                "text": "new"
-            }
-        ),
-        (
-            add_key_to_dict(dict, "five", "new"),
-            None
-        ),
-        (
-            add_key_to_dict(dict, "five.one", "new"),
-            None
-        ),
-        (
-            add_key_to_dict(dict, "five.two", "new"),
-            {
-                "one": 1,
-                "two": "smth",
-                "three": True,
-                "five": {
-                    "one": {
-                        "two": 2,
-                        "test": 3
-                    },
-                    "two": "new"
-                }
-            }
-        ),
-        (
-            add_key_to_dict(dict, "five.one.two", "new"),
-            None
-        ),
-        (
-            add_key_to_dict(dict, "", "new"),
-            {
-                "one": 1,
-                "two": "smth",
-                "three": True,
-                "five": {
-                    "one": {
-                        "two": 2,
-                        "test": 3
-                    }
-                },
-                "": "new"
-            }
-        ),
-        (
-            add_key_to_dict({}, "text", "new"),
-            {
-                "text": "new"
-            }
-        ),
-        (
-            add_key_to_dict({}, "five", "new"),
-            {
-                "five": "new"
-            }
-        ),
-        (
-            add_key_to_dict({}, "five.one", "new"),
-            None
-        ),
-        (
-            add_key_to_dict({}, "five.two", "new"),
-            None
-        ),
-        (
-            add_key_to_dict({}, "five.one.two", "new"),
-            None
-        ),
-        (
-            add_key_to_dict({}, "", "new"),
-            {
-                "": "new"
-            }
-        )
-    ]
-
-}
-
-
-@pytest.mark.parametrize("result, mock", TEST_SETUP["delete"]+TEST_SETUP["change"]+TEST_SETUP["add"])
-def test_utils(result, mock):
-    assert result == mock
+# import pytest
+# from cases_utils import dict_without_key
+# from cases_utils import dict_with_new_key
+# from cases_utils import dict_with_edited_value
+#
+# dict = {
+#     "one": 1,
+#     "two": "smth",
+#     "three": True,
+#     "five": {
+#         "one": {
+#             "two": 2,
+#             "test": 3
+#         }
+#     }
+# }
+#
+# TEST_SETUP = {
+#     "delete": [
+#         # pytest.param(
+#         #     dict_without_key(dict, "text"),
+#         #     None,
+#         #     id='1'
+#         # ),
+#         pytest.param(
+#             dict_without_key(dict, "five"),
+#             {
+#                 "one": 1,
+#                 "two": "smth",
+#                 "three": True
+#             },
+#             id='2'
+#         ),
+#         pytest.param(
+#             dict_without_key(dict, "five.one"),
+#             {
+#                 "one": 1,
+#                 "two": "smth",
+#                 "three": True,
+#                 "five": {}
+#             },
+#             id='3'
+#         ),
+#         pytest.param(
+#             dict_without_key(dict, "five.two"),
+#             None,
+#             id='4'
+#         ),
+#         pytest.param(
+#             dict_without_key(dict, "five.one.two"),
+#             {
+#                 "one": 1,
+#                 "two": "smth",
+#                 "three": True,
+#                 "five": {
+#                     "one": {
+#                         "test": 3
+#                     }
+#                 }
+#             },
+#             id='5'
+#         ),
+#         pytest.param(
+#             dict_without_key(dict, ""),
+#             None,
+#             id='6'
+#         ),
+#         pytest.param(
+#             dict_without_key({}, "text"),
+#             None,
+#             id='7'
+#         ),
+#         pytest.param(
+#             dict_without_key({}, "five"),
+#             None,
+#             id='8'
+#         ),
+#         pytest.param(
+#             dict_without_key({}, "five.one"),
+#             None,
+#             id='9'
+#         ),
+#         pytest.param(
+#             dict_without_key({}, "five.two"),
+#             None,
+#             id='10'
+#         ),
+#         pytest.param(
+#             dict_without_key({}, "five.one.two"),
+#             None,
+#             id='11'
+#         ),
+#         pytest.param(
+#             dict_without_key({}, ""),
+#             None,
+#             id='12'
+#         ),
+#
+#     ],
+#     "change": [
+#         pytest.param(
+#             dict_with_edited_value(dict, "text", "new"),
+#             None,
+#             id='13'
+#         ),
+#         pytest.param(
+#             dict_with_edited_value(dict, "five", "new"),
+#             {
+#                 "one": 1,
+#                 "two": "smth",
+#                 "three": True,
+#                 "five": "new"
+#             },
+#             id='14'
+#         ),
+#         pytest.param(
+#             dict_with_edited_value(dict, "five.one", "new"),
+#             {
+#                 "one": 1,
+#                 "two": "smth",
+#                 "three": True,
+#                 "five": {
+#                     "one": "new"
+#                 }
+#             },
+#             id='15'
+#         ),
+#         pytest.param(
+#             dict_with_edited_value(dict, "five.two", "new"),
+#             None,
+#             id='16'
+#         ),
+#         pytest.param(
+#             dict_with_edited_value(dict, "five.one.two", "new"),
+#             {
+#                 "one": 1,
+#                 "two": "smth",
+#                 "three": True,
+#                 "five": {
+#                     "one": {
+#                         "two": "new",
+#                         "test": 3
+#                     }
+#                 }
+#             },
+#             id='17'
+#         ),
+#         pytest.param(
+#             dict_with_edited_value(dict, "", "new"),
+#             None,
+#             id='18'
+#         ),
+#         pytest.param(
+#             dict_with_edited_value({}, "text", "new"),
+#             None,
+#             id='19'
+#         ),
+#         pytest.param(
+#             dict_with_edited_value({}, "five", "new"),
+#             None,
+#             id='20'
+#         ),
+#         pytest.param(
+#             dict_with_edited_value({}, "five.one", "new"),
+#             None,
+#             id='21'
+#         ),
+#         pytest.param(
+#             dict_with_edited_value({}, "five.two", "new"),
+#             None,
+#             id='22'
+#         ),
+#         pytest.param(
+#             dict_with_edited_value({}, "five.one.two", "new"),
+#             None,
+#             id='23'
+#         ),
+#         pytest.param(
+#             dict_with_edited_value({}, "", "new"),
+#             None,
+#             id='24'
+#         )
+#     ],
+#     "add": [
+#         pytest.param(
+#             dict_with_new_key(dict, "text", "new"),
+#             {
+#                 "one": 1,
+#                 "two": "smth",
+#                 "three": True,
+#                 "five": {
+#                     "one": {
+#                         "two": 2,
+#                         "test": 3
+#                     }
+#                 },
+#                 "text": "new"
+#             },
+#             id='25'
+#         ),
+#         pytest.param(
+#             dict_with_new_key(dict, "five", "new"),
+#             None,
+#             id='26'
+#         ),
+#         pytest.param(
+#             dict_with_new_key(dict, "one", "new"),
+#             None,
+#             id='27'
+#         ),
+#         pytest.param(
+#             dict_with_new_key(dict, "five.one", "new"),
+#             None,
+#             id='28'
+#         ),
+#         pytest.param(
+#             dict_with_new_key(dict, "five.two", "new"),
+#             {
+#                 "one": 1,
+#                 "two": "smth",
+#                 "three": True,
+#                 "five": {
+#                     "one": {
+#                         "two": 2,
+#                         "test": 3
+#                     },
+#                     "two": "new"
+#                 }
+#             },
+#             id='29'
+#         ),
+#         pytest.param(
+#             dict_with_new_key(dict, "five.one.two", "new"),
+#             None,
+#             id='30'
+#         ),
+#         pytest.param(
+#             dict_with_new_key(dict, "", "new"),
+#             {
+#                 "one": 1,
+#                 "two": "smth",
+#                 "three": True,
+#                 "five": {
+#                     "one": {
+#                         "two": 2,
+#                         "test": 3
+#                     }
+#                 },
+#                 "": "new"
+#             },
+#             id='31'
+#         ),
+#         pytest.param(
+#             dict_with_new_key({}, "text", "new"),
+#             {
+#                 "text": "new"
+#             },
+#             id='32'
+#         ),
+#         pytest.param(
+#             dict_with_new_key({}, "five", "new"),
+#             {
+#                 "five": "new"
+#             },
+#             id='33'
+#         ),
+#         pytest.param(
+#             dict_with_new_key({}, "five.one", "new"),
+#             None,
+#             id='34'
+#         ),
+#         pytest.param(
+#             dict_with_new_key({}, "five.two", "new"),
+#             None,
+#             id='35'
+#         ),
+#         pytest.param(
+#             dict_with_new_key({}, "five.one.two", "new"),
+#             None,
+#             id='36'
+#         ),
+#         pytest.param(
+#             dict_with_new_key({}, "", "new"),
+#             {
+#                 "": "new"
+#             },
+#             id='37'
+#         )
+#     ]
+#
+# }
+#
+#
+# @pytest.mark.parametrize("result, mock", TEST_SETUP["delete"])
+# def test_utils_delete(result, mock):
+#     assert result == mock
+#
+#
+# @pytest.mark.parametrize("result, mock", TEST_SETUP["change"])
+# def test_utils_change(result, mock):
+#     assert result == mock
+#
+#
+# @pytest.mark.parametrize("result, mock", TEST_SETUP["add"])
+# def test_utils_add(result, mock):
+#     assert result == mock
